7b3fc9c0078ad66fdec0a334482ece8802205e03
MikhailGzn <127917373+MikhailGzn@users.noreply.github.com>
2023-06-18 22:49:48 -0300

    Delete Python-Portable-3.9.6 directory

--- Python-Portable-3.9.6/apps/Scripts/spyder.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call "%~dp0env.bat"
-if exist "%APPSDIR%\Scripts\spyder.exe" (
-    "%APPSDIR%\Scripts\spyder.exe" %*
-) else (
-    start "" https://www.portabledevapps.net/support-spyder.php
-)
